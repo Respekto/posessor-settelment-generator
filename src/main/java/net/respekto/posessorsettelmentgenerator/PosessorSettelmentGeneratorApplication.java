83094fc7d642005bb18dee186919270a66862224
daniel dabrowski <agrcom@yahoo.pl>
2018-05-12 12:43:36 +0200

    project structure

---
+++ src/main/java/net/respekto/posessorsettelmentgenerator/PosessorSettelmentGeneratorApplication.java
@@ -0,0 +1,12 @@
+package net.respekto.posessorsettelmentgenerator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PosessorSettelmentGeneratorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PosessorSettelmentGeneratorApplication.class, args);
+	}
+}
